0d5785880c73d101dafbe8562c5aa9c81ff66be9
wanyi <wanyile0805@gmail.com>
2022-11-29 10:45:47 -0800

    first commit

---
+++ server/db/dummydb.js
@@ -0,0 +1,458 @@
+export const eventlist = [
+    {
+        id: 1,
+        event: "Onclick",
+        sub: "Alert"
+    },
+    {
+        id: 2,
+        event: "Onclick",
+        sub: "Alert-line-breaks"
+    },
+    {
+        id: 3,
+        event: "Onclick",
+        sub: "Assign-Variables"
+    },
+    {
+        id: 4,
+        event: "Onclick",
+        sub: "Change-Image-src"
+    },
+    {
+        id: 5,
+        event: "Onclick",
+        sub: "Change-Words"
+    },
+    {
+        id: 6,
+        event: "Onclick",
+        sub: "Change-Style"
+    },
+    {
+        id: 7,
+        event: "Onclick",
+        sub: "If-statement"
+    },
+    {
+        id: 8,
+        event: "Onload",
+        sub: "Set-Cookie"
+    },
+    {
+        id: 9,
+        event: "Onmousedown",
+        sub: "Get-Eventype"
+    },
+    {
+        id: 10,
+        event: "Onclick",
+        sub:"Hide-element"
+    },
+    {
+        id: 11,
+        event: "Onclick",
+        sub:"Change-Paragraph"
+    },
+    {
+        id: 12,
+        event: "Onclick",
+        sub: "Window-top-self"
+    },
+    {
+        id: 13,
+        event: "Onclick",
+        sub: "Assign-location"
+    },
+    {
+        id: 14,
+        event: "Onclick-",
+        sub: "Replace-location"
+    },
+    {
+        id: 15,
+        event: "Onclick",
+        sub: "Return-url"
+    },
+    {
+        id: 16,
+        event: "Onclick",
+        sub: "Return-path"
+    },
+    {
+        id: 17,
+        event: "Onclick",
+        sub: "Return-hostevent"
+    },
+    {
+        id: 18,
+        event: "Onclick",
+        sub: "Navigator-cookieEnabled"
+    },
+    {
+        id: 19,
+        event: "Onclick",
+        sub: "Navigator-appCodeevent"
+    },
+    {
+        id: 20,
+        event: "Onclick",
+        sub: "Navigator-product"
+    },
+    {
+        id: 21,
+        event: "Onclick",
+        sub: "Navigator-userAgent"
+    },
+    {
+        id: 22,
+        event: "Onclick",
+        sub: "Navigator-platform"
+    },
+    {
+        id: 23,
+        event: "Onclick",
+        sub: "Navigator-language"
+    },
+    {
+        id: 24,
+        event: "Onclick",
+        sub: "Navigator-javaEnabled"
+    },
+    {
+        id: 25,
+        event: "Onblur",
+        sub: "toUpperCase"
+    },
+    {
+        id: 26,
+        event: "Onchange",
+        sub: "toUpperCase"
+    },
+    {
+        id: 27,
+        event: "Onchange-",
+        sub: "dropdown"
+    },
+    {
+        id: 28,
+        event: "Onclick",
+        sub: "Hello-world"
+    },
+    {
+        id: 29,
+        event: "Onclick",
+        sub: "Display-date"
+    },
+    {
+        id: 30,
+        event: "Ondblclick",
+        sub: "Hello-world"
+    },
+    {
+        id: 31,
+        event: "Onerror",
+        sub: ""
+    },
+    {
+        id: 32,
+        event: "Onfocus",
+        sub: "Change-bgcolor"
+    },
+    {
+        id: 33,
+        event: "Onkeydown",
+        sub: ""
+    },
+    {
+        id: 34,
+        event: "Onkeyup",
+        sub: "Change-color"
+    },
+    {
+        id: 35,
+        event: "Onkeypress",
+        sub: ""
+    },
+    {
+        id: 36,
+        event: "Onkeyup",
+        sub: "Change-value"
+    },
+    {
+        id: 37,
+        event: "Onkeypress",
+        sub: "Event-which"
+    },
+    {
+        id: 38,
+        event: "Onload",
+        sub: "Page"
+    },
+    {
+        id: 39,
+        event: "Onload",
+        sub: "image"
+    },
+    {
+        id: 40,
+        event: "Onmousedown",
+        sub: "Element"
+    },
+    {
+        id: 41,
+        event: "Onmousedown",
+        sub: "Button"
+    },
+    {
+        id: 42,
+        event: "Onmousedown",
+        sub: "clientX-&-clentY"
+    },
+    {
+        id: 43,
+        event: "Onmousedown",
+        sub: "screenX-&-screenY"
+    },
+    {
+        id: 44,
+        event: "Onmousedown",
+        sub: "Change-color"
+    },
+    {
+        id: 45,
+        event: "Onmouseup-change-color",
+        sub: "Change-color"
+    },
+    {
+        id: 46,
+        event: "Onmousedown-",
+        sub: "ShiftKey"
+    },
+    {
+        id: 47,
+        event: "Onmousemove",
+        sub: ""
+    },
+    {
+        id: 48,
+        event: "Onmouseout",
+        sub: ""
+    },
+    {
+        id: 49,
+        event: "Onmouseover",
+        sub: "Usemap"
+    },
+    {
+        id: 50,
+        event: "Onmouseover",
+        sub: "Change-color"
+    },
+    {
+        id: 51,
+        event: "Onmouseenter",
+        sub: "Change-size"
+    },
+    {
+        id: 52,
+        event: "Onmouseleave-change-size",
+        sub: "Change-size"
+    },
+    {
+        id: 53,
+        event: "Onreset",
+        sub: ""
+    },
+    {
+        id: 54,
+        event: "Onclick",
+        sub: "Window-top-self"
+    },
+    {
+        id: 55,
+        event: "Onselect",
+        sub: ""
+    },
+    {
+        id: 56,
+        event: "Onsubmit",
+        sub: ""
+    },
+    {
+        id: 57,
+        event: "Onplay",
+        sub: ""
+    },
+    {
+        id: 58,
+        event: "Onpause",
+        sub: ""
+    },
+    {
+        id: 59,
+        event: "Onclick",
+        sub: "Prompt"
+    },
+    {
+        id: 60,
+        event: "Onclick",
+        sub: "Screen-properties"
+    },
+    {
+        id: 61,
+        event: "Onclick",
+        sub: "Show-hidden"
+    },
+    {
+        id: 62,
+        event: "Onclick",
+        sub: "Open-window"
+    },
+    {
+        id: 63,
+        event: "Onclick",
+        sub: "Close-window"
+    },
+    {
+        id: 64,
+        event: "Onclick",
+        sub: "Open-customed-window"
+    },
+    {
+        id: 65,
+        event: "Onclick-",
+        sub: "Window-moveto"
+    },
+    {
+        id: 66,
+        event: "Onclick",
+        sub: "Window-print"
+    },
+    {
+        id: 67,
+        event: "Onclick",
+        sub: "Window-resizeBy"
+    },
+    {
+        id: 68,
+        event: "Onclick",
+        sub: "Window-resizeTo"
+    },
+    {
+        id: 69,
+        event: "Onclick",
+        sub: "Window-scrollBy"
+    },
+    {
+        id: 70,
+        event: "Onclick",
+        sub: "Window-scrolltTo"
+    },
+    {
+        id: 71,
+        event: "Onclick-",
+        sub: "Window-console.log"
+    },
+    {
+        id: 72,
+        event: "Ondragstart",
+        sub: ""
+    },
+    {
+        id: 73,
+        event: "Ondragend",
+        sub: ""
+    },
+    {
+        id: 74,
+        event: "Ondragenter",
+        sub: ""
+    },
+    {
+        id: 75,
+        event: "Ondragleave",
+        sub: ""
+    },
+    {
+        id: 76,
+        event: "Ondragover",
+        sub: ""
+    },
+    {
+        id: 77,
+        event: "Ondrop",
+        sub: ""
+    },
+    {
+        id: 78,
+        event: "Onscroll",
+        sub: ""
+    },
+    {
+        id: 79,
+        event: "Onsearch",
+        sub: ""
+    },
+    {
+        id: 80,
+        event: "Ontoggle",
+        sub: ""
+    },
+    {
+        id: 81,
+        event: "Oninput",
+        sub: ""
+    },
+    {
+        id: 82,
+        event: "Onended",
+        sub: ""
+    },
+    {
+        id: 83,
+        event: "Onfocusin",
+        sub: ""
+    },
+    {
+        id: 84,
+        event: "Onfocusout",
+        sub: ""
+    },
+    {
+        id: 85,
+        event: "Oncopy",
+        sub: ""
+    },
+    {
+        id: 86,
+        event: "Oninvalid",
+        sub: ""
+    },
+    {
+        id: 87,
+        event: "Onseeked",
+        sub: ""
+    },
+    {
+        id: 88,
+        event: "Onvolumechange",
+        sub: ""
+    },
+    {
+        id: 89,
+        event: "Ontimeupdate",
+        sub: ""
+    },
+    {
+        id: 90,
+        event: "Onseeking",
+        sub: ""
+    },
+    {
+        id: 91,
+        event: "Onratechange",
+        sub: ""
+    },
+]
+
